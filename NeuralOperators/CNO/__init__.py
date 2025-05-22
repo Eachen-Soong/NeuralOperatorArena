6bfe3ceee0f2aac9cbd9ce6d41abeb9e3748f4a9
Eachen Soong <stopratracingplz@gmail.com>
2025-05-22 21:05:19 +0800

    Delete NeuralOperators directory

--- NeuralOperators/CNO/__init__.py
+++
@@ -1,2 +0,0 @@
-from .CNO1d import CNO1d
-from .CNO2d import CNO2d
--- NeuralOperators/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .GNOT import GNOT
-from .tfno.models import FNO, F_FNO2D, LSM_2D, FNO_2D_Original, FNO_Original, ProdFNO_2D_Original, FNO_1D_Original, ProdFNO_1D_Original, DimFNO
-from .CNO import CNO1d, CNO2d 
