68a500f626e327da00b5dd6448c360356bef35a9
eachen <stopratracingplz@gmail.com>
2025-05-22 19:10:38 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # from .GNOT import GNOT
-from .neuralop.models import FNO, F_FNO2D, LSM_2D, FNO_2D_Original, FNO_Original, ProdFNO_2D_Original, FNO_1D_Original, ProdFNO_1D_Original, DimFNO
+from .tfno.models import FNO, F_FNO2D, LSM_2D, FNO_2D_Original, FNO_Original, ProdFNO_2D_Original, FNO_1D_Original, ProdFNO_1D_Original, DimFNO
 from .CNO import CNO1d, CNO2d 
